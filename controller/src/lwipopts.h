b7996d110a8cebfac9cfae2a900f596c33ba871f
Aaro Lehtoaho <aaro.lehtoaho.al@gmail.com>
2026-01-12 20:09:51 +0200

    Changed MEM_SIZE

--- controller/src/lwipopts.h
+++ controller/src/lwipopts.h
@@ -1,19 +1,30 @@
 #ifndef LWIPOPTS_H
 #define LWIPOPTS_H
 
-// Basic lwIP configuration for Pico W
-#define LWIP_SOCKET           1
-#define LWIP_NETCONN          0
-#define LWIP_TIMEVAL_PRIVATE  0
-#define MEM_ALIGNMENT         4
-#define MEM_SIZE             4000
-#define MEMP_NUM_PBUF         8
-#define MEMP_NUM_NETBUF       4
-#define LWIP_ARP              1
-#define LWIP_ICMP             1
-#define LWIP_UDP              1
-#define LWIP_TCP              1
-#define TCP_WND             2048
-#define TCP_MSS              536
+#define NO_SYS                 0
+#define SYS_LIGHTWEIGHT_PROT   1
 
-#endif // LWIPOPTS_H
+#define LWIP_NETCONN           1
+#define LWIP_SOCKET            0
+
+#define LWIP_DHCP              1
+#define LWIP_AUTOIP            0
+
+#define MEM_ALIGNMENT          4
+#define MEM_SIZE               (32 * 1024)
+
+#define MEMP_NUM_PBUF          16
+#define MEMP_NUM_NETBUF        8
+
+#define LWIP_ARP               1
+#define LWIP_ICMP              1
+#define LWIP_UDP               1
+#define LWIP_TCP               1
+
+#define TCP_WND                2048
+#define TCP_MSS                536
+
+#define LWIP_NETIF_HOSTNAME    1
+#define LWIP_NETIF_API         1
+
+#endif
