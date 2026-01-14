6b478a9ae116cee0ef66481f27350db8625aad7a
Aaro Lehtoaho <aaro.lehtoaho.al@gmail.com>
2026-01-14 14:14:17 +0200

    Added test_server.py for testing receiving data over WiFi

---
+++ controller/test_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import socket
+
+HOST = ''
+PORT = 8000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server listening on port {PORT}...")
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print("Received:", data.decode())
