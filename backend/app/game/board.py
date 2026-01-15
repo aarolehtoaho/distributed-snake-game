c0729dd2b5c6fe1114e15f9884ff31bc2904dd61
aarolehtoaho <aaro.lehtoaho.al@gmail.com>
2026-01-15 18:05:10 +0200

    Created game board

---
+++ backend/app/game/board.py
@@ -0,0 +1,45 @@
+from random import choice
+
+class Board:
+    # Square types
+    EMPTY = 0
+    SNAKE = 1
+    FOOD = 2
+
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        self.grid = [[Board.EMPTY for _ in range(width)] for _ in range(height)]
+
+    def place_snake(self, snake_positions: list):
+        for x, y in snake_positions:
+            self.grid[y][x] = Board.SNAKE
+
+    def place_food(self, food_position: tuple):
+        x, y = food_position
+        self.grid[y][x] = Board.FOOD
+
+    def clear_square(self, position: tuple):
+        x, y = position
+        self.grid[y][x] = Board.EMPTY
+
+    def get_square(self, position: tuple):
+        x, y = position
+        return self.grid[y][x]
+
+    def find_empty_square(self) -> tuple | None:
+        empty_squares = []
+        for y in range(self.height):
+            for x in range(self.width):
+                if self.grid[y][x] == Board.EMPTY:
+                    empty_squares.append((x, y))
+        if not empty_squares:
+            return None
+
+        return choice(empty_squares)
+
+    def to_string(self):
+        representation = ""
+        for row in self.grid:
+            representation += ' '.join(str(square) for square in row) + "\n"
+        return representation
