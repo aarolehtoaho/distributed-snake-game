97070b42b6166db2a2a8ce3464aaebdeb82eb0ec
Aaro Lehtoaho <aaro.lehtoaho.al@gmail.com>
2026-01-16 13:56:23 +0200

    Created Client to request state with GET and call renderer functions

---
+++ client/client.py
@@ -0,0 +1,48 @@
+import requests
+import renderer
+
+class Client:
+    HOST = '127.0.0.1'
+    PORT = 8000
+
+    BOARD_WIDTH = 40
+    BOARD_HEIGHT = 30
+
+    def __init__(self):
+        self.renderer = renderer.Renderer(self.BOARD_WIDTH, self.BOARD_HEIGHT)
+        self.state: dict | None = None
+
+    def request_game_state(self) -> dict | None:
+        try:
+            response = requests.get(f'http://{self.HOST}:{self.PORT}/')
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException:
+            return None
+
+    def update(self):
+        self.renderer.clear_screen()
+        self.renderer.draw_grid(self.BOARD_HEIGHT, self.BOARD_WIDTH)
+
+        self.state = self.request_game_state()
+
+        if self.state is None:
+            self.renderer.draw_text_panel('Server is not available')
+            self.renderer.update_display()
+            return
+        
+        food_position: tuple = self.state.get('food_position')
+        snake_position: list[tuple] = self.state.get('snake_position')
+        score: int = self.state.get('score')
+        game_status: str = self.state.get('game_status')
+
+        if not all([food_position, snake_position, score, game_status]):
+            self.renderer.draw_text_panel('Invalid game state received')
+            self.renderer.update_display()
+            return
+        
+        self.renderer.draw_square(food_position[1], food_position[0])
+        for snake_square in snake_position:
+            self.renderer.draw_square(snake_square[1], snake_square[0])
+        self.renderer.draw_text_panel(f'Score: {score} | Status: {game_status}')
+        self.renderer.update_display()
